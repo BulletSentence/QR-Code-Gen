cda64a45251bc6322c3ef9ba14fbb8a61909694b
Leonardo Almeida de Araújo <37451620+BulletSentence@users.noreply.github.com>
2020-04-28 14:22:50 -0300

    Atualizado a correção de erros e alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    final String qr_text = '123456';
+    final String qr_text = 'Almeida';
 
     return Scaffold (
       appBar: AppBar(
@@ -32,6 +32,7 @@ class HomePage extends StatelessWidget{
           children: <Widget>[
             SizedBox(height: 16,),
             Text("Codigo Gerado: $qr_text",
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 30,
               ),
@@ -40,6 +41,8 @@ class HomePage extends StatelessWidget{
             QrImage(
               data: qr_text,
               size: 300,
+              gapless: true,
+              errorCorrectionLevel: QrErrorCorrectLevel.Q,
             ),
           ],
         ),
